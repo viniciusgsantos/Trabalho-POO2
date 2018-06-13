52c8f4447f036eb212916f63746bfb8f249f7768
Vinicius Guedes <vinicius_gs_lp@hotmail.com>
2018-06-13 15:12:25 -0300

    Adicionada funcao que le a resposta da requisicao http e transforma o json em uma string

--- BolsaDeValores/src/br/ufsc/cco/request/Request.java
+++ BolsaDeValores/src/br/ufsc/cco/request/Request.java
@@ -1,9 +1,13 @@
 package br.ufsc.cco.request;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Scanner;
 
 import com.google.gson.Gson;
 
@@ -14,11 +18,23 @@ public class Request {
 			URL url = new URL("https://www.alphavantage.co/query?" + params);
 			HttpURLConnection connection = (HttpURLConnection) url.openConnection();
 			connection.setRequestMethod("GET");
-
-			System.out.println("Resposta: " + connection.getResponseMessage());
-			System.out.println("Status: " + connection.getResponseCode());
+			connection.setRequestProperty("Content-Type", "application/json");
 			
+			InputStreamReader isr = new InputStreamReader(connection.getInputStream());
+			Scanner scanner = new Scanner(isr);
+			StringBuilder builder = new StringBuilder();
+			
+			while(scanner.hasNextLine()) {
+				builder.append("\n");
+				builder.append(scanner.nextLine());
+			}
+			
+			String json = builder.toString();
+			System.out.println(json);
 			Gson gson = new Gson();
+			
+			scanner.close();
+			connection.disconnect();
 			return "" + connection.getResponseCode();
 		} catch (MalformedURLException e) {
 			e.printStackTrace();
