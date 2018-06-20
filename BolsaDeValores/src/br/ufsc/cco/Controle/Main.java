f45938067fac86a146f1a73ceda47cb292826497
Vinicius Guedes dos Santos <vinicius_gs_lp@hotmail.com>
2018-06-20 15:50:23 -0300

    adicionados testes para todos os ativos

--- BolsaDeValores/src/br/ufsc/cco/Controle/Main.java
+++ BolsaDeValores/src/br/ufsc/cco/Controle/Main.java
@@ -9,5 +9,29 @@ public class Main {
 		double ultimoFechamento = ma.getLastClose(json);
 		System.out.println("SMA: " + media);
 		System.out.println("close: " + ultimoFechamento);
+		
+		String json1 = ControladorRequests.getInstance().request(ControladorRequests.GOOGLE);
+		double media1 = ma.getSMA(json1);
+		double ultimoFechamento1 = ma.getLastClose(json1);
+		System.out.println("SMA: " + media1);
+		System.out.println("close: " + ultimoFechamento1);
+		
+		String json2 = ControladorRequests.getInstance().request(ControladorRequests.PETROBRAS);
+		double media2 = ma.getSMA(json2);
+		double ultimoFechamento2 = ma.getLastClose(json2);
+		System.out.println("SMA: " + media2);
+		System.out.println("close: " + ultimoFechamento2);
+		
+		String json3 = ControladorRequests.getInstance().request(ControladorRequests.SANTANDER);
+		double media3 = ma.getSMA(json3);
+		double ultimoFechamento3 = ma.getLastClose(json3);
+		System.out.println("SMA: " + media3);
+		System.out.println("close: " + ultimoFechamento3);
+		
+		String json4 = ControladorRequests.getInstance().request(ControladorRequests.VALE);
+		double media4 = ma.getSMA(json4);
+		double ultimoFechamento4 = ma.getLastClose(json4);
+		System.out.println("SMA: " + media4);
+		System.out.println("close: " + ultimoFechamento4);
 	}
 }
